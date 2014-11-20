f8c0e584042e3628cff0ec45c0ffb5d675545228
knight9z <nv2anh@gmail.com>
2014-11-20 10:13:28 +0700

    creATE

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Frog").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerpos = player.position;
+		playerpos.z = transform.position.z;
+		playerpos.y = transform.position.y;
+		transform.position = playerpos;
+	}
+}
---
+++ Assets/FrogJamp.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogJamp : MonoBehaviour {
+
+	Animator animator;
+	public float speed = 25;
+	public float forwardSpeed = 50;
+	bool didFlap = false;
+
+	bool death = false;
+	// Use this for initialization
+	void Start () {
+		animator = transform.GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//
+		if (death)
+						return;
+		if (Input.GetKey (KeyCode.Space)) {
+			forwardSpeed += Time.deltaTime*speed;
+			
+		}
+
+		if(Input.GetKeyUp(KeyCode.Space)){
+			animator.SetTrigger("doJamp");
+			rigidbody2D.AddForce(Vector2.right*forwardSpeed);
+			rigidbody2D.AddForce(Vector2.up*forwardSpeed*2);
+			didFlap = true;
+		}
+
+		if(didFlap == true){
+			forwardSpeed = 50;
+			didFlap = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		animator.SetTrigger("die");
+	}
+}
